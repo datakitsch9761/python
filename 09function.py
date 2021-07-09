e2cbfd1be0a867ff59e7c71f28a76cb8d5a2519b
datakitsch9761 <data.kitsch@gmail.com>
2021-07-09 12:48:01 +0900

    파이썬 함수 고급

--- 09function.py
+++ 09function.py
@@ -51,3 +51,137 @@ a = input('a는? ')
 b = input('b는? ')
 
 add()
+
+#전역 global 변수와 local 변수
+
+num = 10
+print('전역변수 num', num)
+
+def local():
+    num = 20
+    print('함수내 num', num)
+
+local()
+print('전역변수 num',num) #10
+
+#함수내에서 전역변수 사용하기 : global
+num = 10 #10
+print('전역변수 num', num)
+
+def local():
+    global num #함수내에서 전역변수를 수정할 수 있도록 함
+    num = 20 #20
+    print('함수내 num', num)
+
+local()
+print('전역변수 num', num) #10(x) -> 20이라는 전역변수를 사용하고 싶다
+
+
+#웹사이트 방문 누적하기
+count = 10
+
+def countVistor():
+    global count
+    #global count = 0 # 전역변수는 초기화 불가
+    while True:
+        menu = input('1.웹사이트 방문 2.종료\n'
+          '작업을 선택하세요')
+        if menu == '1':
+            count += 1
+            print(f'총 방문횟수 : {count}')
+        elif menu == '2':
+            print('작업을 종료했습니다')
+            break
+
+print(f'전체 방문횟수 : {count}') #전역변수를 출력한다
+
+countVistor()
+
+
+x = 10
+y = 10
+
+def add():
+    print(x + y)
+
+def add(x,y):
+    print(x + y)
+
+add()
+add(10,10)
+
+#함수의 매개변수 갯수를 동적으로 정의
+#매개변수명 앞에 *로 정의해서 함수를 만들면 됨
+
+#ex) 기존에 만든 add 함수는 2개의 매개변수만 받음
+#2개이상의 매개변수를 받아 결과를 출력하고 싶다면
+def add(x,y,z):
+    print(x + y + z)
+
+def add2(*num):
+    sum = 0
+    for v in num:
+        sum += v
+    print(sum)
+
+add2(10,10,10)
+add2(10,10,10,10,10)
+add2(10,10,10,10,10,10,10,10,10)
+
+#MMS / SMS 구분하기
+def computeMS(msg):
+    msgSize = len(msg)
+    print('입력하신 문자열 길이', msgSize)
+
+    if msgSize <=100:
+        print('단문메세지로 50원이 부과됩니다.')
+    elif msgSize > 100 :
+        print("장문제세지로 100원이 부가됩니다.")
+
+msg = input('문자열을 입력하세요')
+computeMS(msg)
+
+#함수정의시 매개변수를 선언했지만
+#함수호출시 인수를 순서대로 입력하지 않을 경우
+# => 인수값 앞에 매개변수명을 지정
+def computeSunjJuk(name,kor,eng,mat):
+    tot = kor + eng + mat
+    avg = tot / 3
+    print(tot,avg)
+
+computeSunjJuk('혜교', 98,78,65)
+computeSunjJuk(98,78,65,'수지') #오류발생
+computeSunjJuk(98,78,65, name = '수지') #오류발생
+
+computeSunjJuk(kor=98,eng=78,mat=65, name = '수지')
+computeSunjJuk(name='지현',kor=78,eng=65,mat=99)
+
+# 매개변수를 정의했지만
+# 매개변수 없이 함수 호출하고 싶다면?
+# -> 매개변수 선언시 초기값을 지정함
+
+def add3(x=10,y=10):
+    print(x + y)
+
+add3(5,5)
+add3()
+
+#사칙연산 프로그램
+
+def compute(x,y):
+    add = x + y
+    min = x - y
+    mul = x * y
+    div = x / y
+
+    return add,min,mul,div
+
+num1 = int(input('첫번째 정수를 입력하세요'))
+num2 = int(input('두번째 정수를 입력하세요'))
+p,m,c,d = compute(num1, num2)
+result = compute(num1,num2)
+
+print(f'사칙연산 결과 : ({p},{m},{c},{d:.2f})')
+print(f'사직연산 결과 : {result}')
+
+
