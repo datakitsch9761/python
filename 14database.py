553436b38d00cc6b1e164cb3ff3edaeb9bd4cacc
datakitsch9761 <data.kitsch@gmail.com>
2021-07-13 18:12:43 +0900

    파이썬 데이터 베이스

--- 14database.py
+++ 14database.py
@@ -34,7 +34,7 @@ conn.close()
 # mysql를 이용한 데이터베이스 활용하기
 import pymysql
 
-conn = pymysql.connect(host='bigdata.cfedxhiyqmff.ap-northeast-2.rds.amazonaws.com',
+conn = pymysql.connect(host='bigdata.csv7boydpoeg.ap-northeast-2.rds.amazonaws.com',
                        charset='utf8', user='playground',
                        password='bigdata2020', db='playground')
 
@@ -53,7 +53,105 @@ for row in rows:
 
 
 # 직책이 IT_PROG인 사원들의 사번, 성, 입사일을 출력하는 코드 작성
+import pymysql
 
+conn = pymysql.connect(host='bigdata.csv7boydpoeg.ap-northeast-2.rds.amazonaws.com',
+                       charset='utf8', user='playground',
+                       password='bigdata2020', db='playground')
+
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+sql = " select EMPLOYEE_ID, LAST_NAME, Hire_DATE from EMPLOYEES " \
+      " where job_id = 'IT_PROG' "
+
+cursor.execute(sql)
+rows = cursor.fetchall()
+
+cursor.close()
+conn.close()
+
+for row in rows:
+    print(row['EMPLOYEE_ID'], row['LAST_NAME'], row['Hire_DATE'])
 
 
 # 30번 부서의 사원수를 조회하는 코드 작성
+
+conn = pymysql.connect(host='bigdata.csv7boydpoeg.ap-northeast-2.rds.amazonaws.com',
+                       charset='utf8', user='playground',
+                       password='bigdata2020', db='playground')
+
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+sql = ' select count(LAST_NAME) cnt from EMPLOYEES '\
+      ' where DEPARTMENT_ID = 30 '
+
+cursor.execute(sql)
+rows = cursor.fetchone()
+
+cursor.close()
+conn.close()
+
+# 단일값이므로 for문 없이 바로 출력
+print(rows['cnt'])
+
+
+
+# 데이터 삽입
+# 데이터베이스 연결
+import sqlite3
+conn = sqlite3.connect('c:/java/sqlite3/siestageek.db')
+
+# 접속후 커서를 가져옴
+cur = conn.cursor()
+
+# 질의문 작성 I
+pcode = '12345'
+pname = '에어컨'
+price = 35000
+amount = 10
+sql = ' insert into product values ' \
+      f' ("{pcode}","{pname}",{price},{amount}) '
+
+# 질의문 실행 I
+cur.execute(sql)
+
+
+# 질의문 작성 II - sqlite3 미지원
+sql = ' insert into product values (%s, %s, %s, %s) '
+param = (pcode, pname, price, amount)
+
+# 질의문 실행 II
+cur.execute(sql, param)
+
+
+# 실행한 내용을 서버에 반영
+conn.commit()
+
+# 작업종료
+cur.close()
+conn.close()
+
+
+# 이름,국어,영어,수학을 입력받아
+# sungjuk 테이블에 저장
+conn = pymysql.connect(host='bigdata.cfedxhiyqmff.ap-northeast-2.rds.amazonaws.com',
+                       charset='utf8', user='playground',
+                       password='bigdata2020', db='playground')
+
+cursor = conn.cursor()
+
+name = input('이름은? ')
+kor = input('국어는? ')
+eng = input('영어는? ')
+mat = input('수학은? ')
+sql = ' insert into sungjuk (name,kor,eng,mat) ' \
+      ' values (%s, %s, %s, %s) '
+param = (name, kor, eng, mat)
+
+cnt = cursor.execute(sql, param)
+print('입력한 데이터 건수 : ', cnt)
+
+conn.commit()
+
+cursor.close()
+conn.close()
